13b080646254ff8c207d06ea3178003e4e3ecca3
262010 <262010@WINMAC-7>
2023-10-21 14:32:54 +0300

    upd

---
+++ NBApp/Pages/PlayerDetails.xaml.cs
@@ -0,0 +1,38 @@
+﻿using NBApp.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NBApp.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PlayerDetails.xaml
+    /// </summary>
+    public partial class PlayerDetails : Page
+    {
+        PlayerInTeam contextPlayersInTeam;
+        public PlayerDetails(PlayerInTeam playerInTeam)
+        {
+            InitializeComponent();
+            App.TitlePage = "Player Detail";
+            contextPlayersInTeam = playerInTeam;
+            DataContext = contextPlayersInTeam;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new MainVisitorPage());
+        }
+
+    }
+}
--- NBApp/Pages/PlayersMain.xaml.cs
+++ NBApp/Pages/PlayersMain.xaml.cs
@@ -21,17 +21,20 @@ namespace NBApp.Pages
     /// </summary>
     public partial class PlayersMain : Page
     {
-        public PlayersMain()
-        {
-            InitializeComponent();
-            CbSeason.ItemsSource = App.DB.Season.ToList();
-            CbTeam.ItemsSource = App.DB.Team.ToList();
-            Refresh();
-        }
         int maxPage = 0;
         int numberPage = 0;
         int count = 10;
         int fakePage = 1;
+        public PlayersMain()
+        {
+            InitializeComponent();
+            App.TitlePage = "Players Main";
+            CbSeason.ItemsSource = App.DB.Season.ToList();
+            CbTeam.ItemsSource = App.DB.Team.ToList();
+            AllRB.IsChecked = true;
+            Refresh();
+        }
+        
 
         private void BtnFirstPage_Click(object sender, RoutedEventArgs e)
         {
@@ -78,14 +81,24 @@ namespace NBApp.Pages
         {
             IEnumerable<PlayerInTeam> filterCase = App.DB.PlayerInTeam;
 
-            //if(CbSeason.SelectedItem != null)
-            //{
-            //    filterCase = filterCase.Where(x => x.Season == CbSeason.SelectedItem);
-            //}
-            //if (CbTeam.SelectedItem != null)
-            //{
-            //    filterCase = filterCase.Where(x => x.Team == CbTeam.SelectedItem);
-            //}
+            if (CbSeason.SelectedItem != null)
+            {
+                filterCase = filterCase.Where(x => x.Season == CbSeason.SelectedItem);
+            }
+            if (CbTeam.SelectedItem != null)
+            {
+                filterCase = filterCase.Where(x => x.Team == CbTeam.SelectedItem);
+            }
+
+            foreach (var item in SortLettersSt.Children)
+            {
+                var RadioButton = item as RadioButton;
+                if (RadioButton.IsChecked == true)
+                {
+                    filterCase = filterCase.Where(x => x.Player.Name.StartsWith($"{RadioButton.Content}"));
+                }
+            }
+
             if (filterCase.Count() > count)
             {
                 if (filterCase.Count() % count > 0)
@@ -106,15 +119,17 @@ namespace NBApp.Pages
                 fakePage = maxPage;
             }
 
-            //if(TbPlayername.Text.Length > 0)
-            //{
-            //    filterCase = filterCase.Where(x => x.Player.Name.ToLower().Contains(TbPlayername.Text.ToLower()));
-            //}
+            if (TbPlayername.Text.Length > 0)
+            {
+                filterCase = filterCase.Where(x => x.Player.Name.ToLower().Contains(TbPlayername.Text.ToLower()));
+            }
             filterCase = filterCase.Skip(count * numberPage).Take(count).ToList();
             DtPlayers.ItemsSource = filterCase.ToList();
 
             MaxPage.Text = $"of {maxPage}";
             CurrentPage.Text = fakePage.ToString();
+
+            RecordText.Text = $"Total {maxPage * count} records, {count} in one page";
         }
 
         private void TbPlayername_TextChanged(object sender, TextChangedEventArgs e)
@@ -134,9 +149,24 @@ namespace NBApp.Pages
 
         private void CurrentPage_TextChanged(object sender, TextChangedEventArgs e)
         {
-            //fakePage = int.Parse(CurrentPage.Text);
-            //numberPage = fakePage - 1;
-            //Refresh();
+            fakePage = int.Parse(CurrentPage.Text);
+            numberPage = fakePage - 1;
+            Refresh();
+        }
+
+        private void AllRB_Checked(object sender, RoutedEventArgs e)
+        {
+            Refresh();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new MainVisitorPage());
+        }
+
+        private void ImageBtn_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var selectedPlayer = (sender as Image).DataContext as PlayerInTeam;
+            NavigationService.Navigate(new PlayerDetails(selectedPlayer));
         }
     }
 }
