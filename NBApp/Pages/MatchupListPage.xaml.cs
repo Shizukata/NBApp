de10044eecf8264dd6157c98e5d220746d98e894
262010 <262010@1220-001-22>
2023-10-24 16:48:05 +0300

    Upd

--- NBApp/Pages/MatchupListPage.xaml.cs
+++ NBApp/Pages/MatchupListPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using NBApp.Components;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,6 +26,48 @@ namespace NBApp.Pages
         {
             InitializeComponent();
             LvMatchup.ItemsSource = App.DB.Matchup.ToList();
+            DtSort.SelectedDate = DateTime.Now;
+            App.TitlePage = "Machup List";
+        }
+
+        private void LeftBtn_Click(object sender, RoutedEventArgs e)
+        {
+            DtSort.SelectedDate -= TimeSpan.FromDays(1);
+        }
+
+        private void RightBtn_Click(object sender, RoutedEventArgs e)
+        {
+            DtSort.SelectedDate += TimeSpan.FromDays(1);
+        }
+
+        private void MatchupSearch()
+        {
+            IEnumerable<Matchup> matchups = App.DB.Matchup.OrderBy(x => x.Starttime).Where(x => x.Status == -1);
+            if(DtSort.SelectedDate != null)
+            {
+                matchups = matchups.Where(x => x.Starttime >= DtSort.SelectedDate);
+            }
+
+            var leftTeam = App.DB.Team.FirstOrDefault(x => x.Id == matchups.FirstOrDefault().Team1.Id);
+            ImgTeamName.Source = ConvertImage(leftTeam.MainImage);
+            TbTeamName.Text = leftTeam.TeamName;
+        }
+
+        private BitmapImage ConvertImage(byte[] Image)
+        {
+            var empImage = new BitmapImage();
+            using (var mem = new MemoryStream(Image))
+            {
+                mem.Position = 0;
+                BeginInit();
+                empImage.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                empImage.CacheOption = BitmapCacheOption.OnLoad;
+                empImage.UriSource = null;
+                empImage.StreamSource = mem;
+                empImage.EndInit();
+            }
+            empImage.Freeze();
+            return empImage;
         }
     }
 }
